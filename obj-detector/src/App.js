117994c531c9ded52fa39e09a73f802d22878d1a
Bryan Tor <bterrific2008@gmail.com>
2019-08-27 00:03:43 -0700

    It works

--- obj-detector/src/App.js
+++ obj-detector/src/App.js
@@ -1,96 +1,74 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect, useCallback } from "react";
 import * as cocoSsd from "@tensorflow-models/coco-ssd";
-import breadLocation from "./sandwich-bread.gif";
 
 function App(props) {
   //const modelPromise = cocoSsd.load();
 
-  const [file, setFile] = useState(0);
+  const [file, setFile] = useState(null);
   const [imgClass, setImgClass] = useState("hidden");
-  const [canvasClass, setCanvasClass] = useState("hidden");
   const [isLoading, setIsLoading] = useState(false);
-
-  var canvasRef = useRef(null);
+  const [model, setModel] = useState(null);
+  const [detected, setDetect] = useState("");
 
   useEffect(() => {
-    const loadImage = file => {
-      var img = new Image();
-      img.onload = function() { console.log("Image loaded.");}
-      img.src = file;
-      return img;
-    };
-
-    async function applyBox() {
+    async function loadModel() {
       setIsLoading(true);
-
-      const img = await loadImage(file);
-      const bread = await loadImage(breadLocation);
-
-      if (file !== 0) {
-        const model = await cocoSsd.load();
-
-        const predictions = await model.detect(img);
-
-        const ctx = canvasRef.current.getContext("2d");
-        ctx.save();
-        console.log("Predictions: ", predictions);
-
-        predictions.forEach(prediction => {
-          const breadHeight = (bread.height * prediction.bbox[3]) / bread.width;
-
-          const topBread = [
-            prediction.bbox[0],
-            prediction.bbox[1] - breadHeight / 2,
-            prediction.bbox[2],
-            breadHeight
-          ];
-
-          const bottomBread = [
-            prediction.bbox[0],
-            prediction.bbox[1] - breadHeight / 2 + prediction.bbox[3],
-            prediction.bbox[2],
-            breadHeight
-          ];
-
-          ctx.drawImage(
-            bread,
-            topBread[0],
-            topBread[1],
-            topBread[2],
-            topBread[3]
-          );
-
-          ctx.drawImage(
-            bread,
-            bottomBread[0],
-            bottomBread[1],
-            bottomBread[2],
-            bottomBread[3]
-          );
-        });
-
-      }
-
+      setModel(await cocoSsd.load("mobilenet_v2"));
       setIsLoading(false);
     }
 
-    applyBox();
+    loadModel();
+  }, []);
+
+  useEffect(() => {
+    async function detect() {
+      if (file) {
+        setIsLoading(true);
+
+        const image = new Image();
+        image.src = file;
+
+        image.onload = await async function() {
+          let detectText = "";
+
+          let predictions = await model.detect(image);
+          predictions.forEach(prediction => {
+            detectText = detectText.concat(
+              prediction.class + " " + (prediction.score * 100).toFixed(2) + " "
+            );
+          });
+
+          setDetect(detectText);
+
+          setIsLoading(false);
+        };
+      }
+    }
+
+    detect();
   }, [file]);
 
   const imageUpdate = event => {
     setFile(URL.createObjectURL(event.target.files[0]));
-    setImgClass("coverImage app-position");
-    setCanvasClass("coverCanvas app-position");
+    setImgClass("center-fit");
   };
 
   return (
-    <div>
-      <input type="file" disabled={isLoading} onChange={imageUpdate} />
-      {isLoading && <div>Loading...</div>}
+    <>
+      <input
+        type="file"
+        name="pic"
+        accept="image/*"
+        disabled={isLoading}
+        onChange={imageUpdate}
+        className="selection"
+      />
 
-      <img src={file} className={imgClass} />
-      <canvas ref={canvasRef} className={canvasClass} />
-    </div>
+      <div className="imgBox">
+        {!file ? "No image" : <img src={file} className={imgClass} />}
+      </div>
+      <div>{isLoading ? "Loading..." : detected}</div>
+    </>
   );
 }
 
